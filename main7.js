98335997394b3b17789038527784acac522730b0
Kingslayer13 <3700511l>
2013-11-21 20:20:23 +0200

    примеры работы [x]+

--- main7.js
+++ main7.js
@@ -8,7 +8,9 @@
     console.log("mama ss".match(/[as]/g)); // ["a", "a", "s", "s"]
 
 
-
-
+    console.log("mamla121".match(/[as]\d/g)); //a1
+    console.log("aaaa".match(/[as]+/g)); // aaaa
+    console.log("aaaa".match(/[as]\d/g)); // null
+    console.log("mamla121".match(/[as]\d+/g)); //a121
 
 })()
